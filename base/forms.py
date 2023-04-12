5df200329d96a1d226c6ca811240ede883e445cb
HanzCC <hanze.t.gmail.com>
2023-04-12 21:46:50 +0800

    work done 120423

--- base/forms.py
+++ base/forms.py
@@ -1,6 +1,11 @@
 from django.forms import ModelForm
-from .models import Room
-from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import User, Room
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['name', 'username', 'email', 'password1', 'password2']
 
 ## Create form for the Room
 class RoomForm(ModelForm):
@@ -14,4 +19,4 @@ class RoomForm(ModelForm):
 class UserForm(ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['avatar','name', 'username', 'email', 'bio']
--- base/models.py
+++ base/models.py
@@ -5,7 +5,14 @@ from django.contrib.auth.models import AbstractUser
 # # Create your models here.
 
 class User(AbstractUser):
-    pass
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True, null=True)
+    bio = models.TextField(null=True)
+
+    avatar = models.ImageField(null=True, default='avatar.svg')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
 # A topic can has multiple rooms, but a room only can has 1 topic 
 class Topic(models.Model):
